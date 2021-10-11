b3b75dc7b9acedbf3182acf5c684ef45cd64e808
Kunal Shaw <78487132+KunalShaw498@users.noreply.github.com>
2021-10-12 00:53:55 +0530

    new

---
+++ backend/data.js
@@ -0,0 +1,88 @@
+import bcrypt from "bcryptjs";
+
+const data = {
+  users: [
+    {
+      name: "Kunal",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Raunak",
+      email: "user@example.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: "Nike Slim Shirt ",
+      category: "Shirts",
+      image: "/images/p1.jpg",
+      price: 120,
+      countInStock: 70,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "High quality product",
+    },
+    {
+      name: "Adidas Slim Shirt ",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 140,
+      countInStock: 100,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 14,
+      description: "High quality product",
+    },
+    {
+      name: "Puma Slim Shirt ",
+      category: "Shirts",
+      image: "/images/p3.jpg",
+      price: 80,
+      countInStock: 0,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 21,
+      description: "High quality product",
+    },
+    {
+      name: "Asics Slim Shirt ",
+      category: "Pants",
+      image: "/images/p4.jpg",
+      price: 220,
+      countInStock: 19,
+      brand: "Asics",
+      rating: 4.8,
+      numReviews: 8,
+      description: "High quality product",
+    },
+    {
+      name: "New Balance Slim Shirt ",
+      category: "Pants",
+      image: "/images/p5.jpg",
+      price: 160,
+      countInStock: 25,
+      brand: "New Balance",
+      rating: 3.7,
+      numReviews: 11,
+      description: "High quality product",
+    },
+    {
+      name: "Fila Slim Shirt ",
+      category: "Pants",
+      image: "/images/p6.jpg",
+      price: 60,
+      countInStock: 30,
+      brand: "Fila",
+      rating: 4.6,
+      numReviews: 31,
+      description: "High quality product",
+    },
+  ],
+};
+
+export default data;
